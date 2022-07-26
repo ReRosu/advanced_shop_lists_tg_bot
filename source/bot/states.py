78b9efb55e5e2238de7b06a6b8abbc4f00814e14
rerosu <rosutamu.afanasev@gmail.com>
2022-07-26 17:26:57 +0500

    add bot states

--- source/bot/states.py
+++ source/bot/states.py
@@ -1,1 +1,18 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class CreateShopListStates (StatesGroup):
+    writing_shop_list = State()
+    adding_friends = State()
+    giving_name = State()
+    checking_shop_list = State()
+
+
+class ChangingShopListStates (StatesGroup):
+    noticing_purchase = State()
+    changing_added_friends = State()
+    checking_shop_list = State()
+
+
+class AddingFriendStates (StatesGroup):
+    writing_friend_id = State()
